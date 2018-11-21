8d79cf9be9c4eb070754b4c7ff88ea215c241582
Paul Gain <paul.gain@javexconsulting.com>
2018-11-21 15:43:31 +0000

    Set the port to an env var called PORT
    Heroku is expecting the node app port to be set on the PORT environment
    variable.

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const app = require('app');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Investment projects app started, listening on port ${port}!`));
