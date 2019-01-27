a7139d19a5fd724009a89af5ae56f42512514bcd
Paul Gain <paul.gain@javexconsulting.com>
2019-01-27 14:42:53 +0000

    Adding the ability to delete an auto-complete field.
    This piece of functionality allows a user to remove any previously added
    client contacts.

--- app/assets/js/AddAnotherClient.js
+++ app/assets/js/AddAnotherClient.js
@@ -1,49 +1,104 @@
 import {autoComplete} from "./auto-complete-utils";
 import $ from "jquery";
 
-const CLIENT_CONTACT = 'client-contact';
 const HYPHEN = '-';
+const DELETE_SELECTOR = '.auto-complete-delete';
+const CLIENT_CONTACT = 'client-contact';
+const SHOW_DELETE_BUTTON = `show-delete-button`;
+const ADD_ANOTHER_CLIENT_SELECTOR = `.add-another-client`;
+const CLIENT_CONTACTS_SELECTOR = `div[id^='${CLIENT_CONTACT}']`;
 
 class AddAnotherClient {
   constructor() {
-    this.$addAnotherButton = $('.add-another');
-    this.addEventListeners();
+    this.$addAnotherButton = $(ADD_ANOTHER_CLIENT_SELECTOR);
+    if(this.$addAnotherButton.length) {
+      this.addEventListeners();
+    }
   }
 
   addEventListeners() {
-    if(this.$addAnotherButton.length) {
-      this.$addAnotherButton.click(this.onAddAnotherButtonClick.bind(this));
+    // Add an event listener for when a user clicks 'Add another client contact'.
+    this.$addAnotherButton.click(this.onAddAnotherButtonClick.bind(this));
+
+    const clientContacts = document.querySelectorAll(CLIENT_CONTACTS_SELECTOR);
+    if (clientContacts.length > 1) {
+      // Add click event listeners to all delete buttons that maybe showing.
+      // By default the first client contact is not deletable.
+      clientContacts.forEach((clientContact) => new DeleteEventListener(clientContact.id));
     }
   }
 
   onAddAnotherButtonClick() {
-    let index = this.getIndexFromId(this.getLastClientContactId());
-    index++;
+    const clientContacts = $(CLIENT_CONTACTS_SELECTOR);
+    if(clientContacts.length === 1) {
+      // The user wants to add another client contact. We need to ensure
+      // the first client contact becomes deletable.
+      clientContacts.closest(DELETE_SELECTOR).addClass(SHOW_DELETE_BUTTON);
+      new DeleteEventListener(clientContacts.attr('id'));
+    }
 
+    // Ascertain the next id based on the previous one.
+    // An id will look like 'client-contact-n' where n is a number and n > 0
+    const id = this.getLastClientContactId();
+
+    // Retrieve the id number part of the id and increment it.
+    let index = this.getNextIndexFromId(id);
+
+    // The new client contact id.
     this.clientContactId = `${CLIENT_CONTACT}-${index}`;
 
+    // The HTML we are adding to the DOM, it includes a delete button and the
+    // div along with its client contact id that we later pass to auto-complete.
     const html =
-      `<div class="govuk-form-group">
-         <label class="govuk-label" for="${this.clientContactId}">Client contact ${index}</label>
+      `<div class="govuk-form-group auto-complete-delete ${SHOW_DELETE_BUTTON}">
+         <label class="govuk-label" for="${this.clientContactId}">Client contact</label>
          <div id="${this.clientContactId}"></div>
+         <button class="${this.clientContactId}" type="button">
+           <i class="fas fa-user-times"></i>
+         </button>
        </div>`;
 
-    // Add this auto-complete component before the 'Add another' button.
+    // Add the HTML component to the DOM just before the 'Add another' button.
     $(html).insertBefore(this.$addAnotherButton);
 
-    // Select the auto-complete element we've just added and pass to autocomplete.
-    const element = $(`#${this.clientContactId}`)[0];
+    // Add the delete button event listener.
+    new DeleteEventListener(this.clientContactId);
 
-    // Initialise the auto-complete component
+    // Select the auto-complete element we've just added to the DOM.
+    const clientContactIdSelector = `#${this.clientContactId}`;
+    const element = $(clientContactIdSelector)[0];
+
+    // Initialise the auto-complete component with what we've just selected above.
     autoComplete(element, this.clientContactId,'clients');
   }
 
   getLastClientContactId() {
-    return this.$addAnotherButton.prev().find(`div[id^='${CLIENT_CONTACT}']`).attr('id');
+    return this.$addAnotherButton.prev().find(CLIENT_CONTACTS_SELECTOR).attr('id');
   }
 
-  getIndexFromId(id) {
-    return parseInt(id.substring(id.lastIndexOf(HYPHEN) + 1));
+  getNextIndexFromId(id) {
+    return parseInt(id.substring(id.lastIndexOf(HYPHEN) + 1)) + 1;
+  }
+}
+
+class DeleteEventListener {
+  constructor(id) {
+    this.$deleteButton = $(`.${id}`);
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    if(this.$deleteButton.length) {
+      this.$deleteButton.click(this.onDeleteClick.bind(this));
+    }
+  }
+
+  onDeleteClick() {
+    this.$deleteButton.closest(DELETE_SELECTOR).remove();
+    const deleteElement = $(DELETE_SELECTOR);
+    if(deleteElement.length === 1) {
+      deleteElement.removeClass(SHOW_DELETE_BUTTON);
+    }
   }
 }
 
